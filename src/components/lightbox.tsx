4ee0ea9c4096033963b401475c326ade301649ff
Nina Lisitsinskaya <vvatashi@protonmail.com>
2022-06-10 00:27:30 +0600

    Add lightbox resize

--- src/components/lightbox.tsx
+++ src/components/lightbox.tsx
@@ -15,6 +15,20 @@ export function Lightbox({ visible, file, onClose }: LightboxProps) {
   const [lightboxVisible, setLightboxVisible] = useState(true);
   useEffect(() => setLightboxVisible(false), []);
 
+  const elementRef = useRef<HTMLElement>(null);
+  useEffect(() => {
+    if (elementRef.current === null) {
+      return;
+    }
+
+    function handle(event: WheelEvent) {
+      event.preventDefault();
+    }
+
+    elementRef.current.addEventListener('wheel', handle);
+    return () => elementRef.current?.removeEventListener('wheel', handle);
+  });
+
   const onEnter = useCallback(() => setLightboxVisible(true), []);
   const onExited = useCallback(() => setLightboxVisible(false), []);
 
@@ -44,9 +58,23 @@ export function Lightbox({ visible, file, onClose }: LightboxProps) {
     dragStartPosition.current = data;
   }, []);
 
+  const [transition, setTransition] = useState(false);
+
   const onRndDragStop = useCallback(
     (event: any, data: DraggableData) => {
-      setPosition(data);
+      const minX = Math.min(window.innerWidth - size.width, 0);
+      const minY = Math.min(window.innerHeight - size.height, 0);
+
+      const maxX = Math.max(0, window.innerWidth - size.width);
+      const maxY = Math.max(0, window.innerHeight - size.height);
+
+      const x = Math.max(minX, Math.min(data.x, maxX));
+      const y = Math.max(minY, Math.min(data.y, maxY));
+
+      setPosition({ x, y });
+
+      setTransition(true);
+      setTimeout(() => setTransition(false), 100);
 
       if (
         typeof onClose !== 'undefined' &&
@@ -56,17 +84,51 @@ export function Lightbox({ visible, file, onClose }: LightboxProps) {
         onClose();
       }
     },
-    [onClose]
+    [size, onClose]
   );
 
   const onDragStart = useCallback((event: DragEvent) => {
     event.preventDefault();
   }, []);
 
+  const onWheel = useCallback(
+    (event: React.WheelEvent) => {
+      const scale = event.deltaY > 0 ? 1 / 1.1 : 1.1;
+      setSize((size) => ({ width: size.width * scale, height: size.height * scale }));
+      setPosition((position) => {
+        const relativeX = (event.clientX - position.x) / size.width;
+        const relativeY = (event.clientY - position.y) / size.height;
+
+        const newWidth = size.width * scale;
+        const newHeight = size.height * scale;
+
+        const deltaWidth = newWidth - size.width;
+        const deltaHeight = newHeight - size.height;
+
+        const newX = position.x - deltaWidth * relativeX;
+        const newY = position.y - deltaHeight * relativeY;
+
+        const minX = Math.min(window.innerWidth - size.width, 0);
+        const minY = Math.min(window.innerHeight - size.height, 0);
+
+        const maxX = Math.max(0, window.innerWidth - size.width);
+        const maxY = Math.max(0, window.innerHeight - size.height);
+
+        const x = Math.max(minX, Math.min(newX, maxX));
+        const y = Math.max(minY, Math.min(newY, maxY));
+
+        return { x, y };
+      });
+    },
+    [size]
+  );
+
   const overlayClassName = [
     'lightbox-overlay',
     lightboxVisible ? 'lightbox-overlay_visible' : 'lightbox-overlay_hidden',
   ].join(' ');
+
+  const lightboxClass = ['lightbox', transition ? 'lightbox_transition' : ''].join(' ');
 
   return (
     <CSSTransition
@@ -79,8 +141,7 @@ export function Lightbox({ visible, file, onClose }: LightboxProps) {
     >
       <div className={overlayClassName} ref={nodeRef}>
         <Rnd
-          className="lightbox"
-          bounds="parent"
+          className={lightboxClass}
           position={position}
           size={size}
           enableResizing={false}
@@ -88,7 +149,7 @@ export function Lightbox({ visible, file, onClose }: LightboxProps) {
           onDragStop={onRndDragStop}
         >
           {file !== null && (
-            <picture className="lightbox__picture" onDragStart={onDragStart}>
+            <picture className="lightbox__picture" ref={elementRef} onDragStart={onDragStart} onWheel={onWheel}>
               <img className="lightbox__image" src={file.originalUrl} alt="" />
             </picture>
           )}
